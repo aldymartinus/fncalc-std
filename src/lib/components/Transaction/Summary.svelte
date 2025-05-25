13bae118c1638713ac18af67190c9940222aca96
aldymartinus <18421144.aldy@gmail.com>
2025-05-25 20:47:02 +0700

    feat: summary is now functioning

--- src/lib/components/Transaction/Summary.svelte
+++ src/lib/components/Transaction/Summary.svelte
@@ -1,11 +1,19 @@
+<script>
+    let { currentCart } = $props();
+    const f = (cash) => new Intl.NumberFormat().format(cash);
+    const itemCount = currentCart.length;
+</script>
+
 <div id="cart-summary">
-    <span id="item-count">Item count: 0</span>
-    <span id="current-gt">Grand total: 0</span>
+    <span id="item-count">Item count: {currentCart.length}</span>
+    <span id="current-gt"
+        >Grand total: {f(currentCart.reduce((acc, n) => acc + n.price, 0))}</span
+    >
 </div>
 
 <style>
     #cart-summary {
-        padding: 0.15rem 0;
+        padding: 0.1rem;
         display: flex;
         justify-content: space-between;
         background-color: #e1e7f5;
@@ -13,13 +21,13 @@
 
     #cart-summary span {
         color: #333;
-        font-size: 0.9rem;
+        font-size: 0.8rem;
         padding: 0.2rem;
         font-weight: 700;
     }
 
     #cart-summary > :first-child {
-        margin: 0 0 0 0.1rem;
+        margin: 0 0 0 0.32rem;
     }
 
     #cart-summary > :last-child {
@@ -27,6 +35,10 @@
     }
 
     @media screen and (min-width: 900px) {
+        #cart-summary {
+            padding: 0.15rem 0;
+        }
+
         #cart-summary span {
             margin: 0 auto;
             font-size: 0.9rem;
