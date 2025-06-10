9fde8c7e653378cf931b81613da68e89c558ed16
aldymartinus <18421144.aldy@gmail.com>
2025-06-10 20:41:52 +0700

    wip: working on receipt theme selection and customization

---
+++ src/lib/components/Customize/ReceiptConfig.svelte
@@ -0,0 +1,36 @@
+<div id="config-button-container">
+    <button id="customize-btn">Customize</button>
+    <button id="preview-btn">Preview</button>
+</div>
+
+<style>
+    #config-button-container {
+        width: 100%;
+        display: flex;
+        position: fixed;
+        bottom: 0;
+    }
+
+    #customize-btn {
+        margin: 0 0.2rem 0.2rem 0.2rem;
+    }
+
+    #preview-btn {
+        margin: 0 0.27rem 0.2rem 0;
+    }
+
+    #config-button-container button {
+        padding: 0.7rem;
+        width: 100%;
+    }
+
+    #config-button-container:hover button {
+        cursor: pointer;
+    }
+
+    @media screen and (min-width: 900px) {
+        #config-button-container {
+            width: 35%;
+        }
+    }
+</style>
---
+++ src/lib/components/Customize/ThemeSelect.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+    let selectedStyle = $state();
+    let themeSelection;
+
+    const handleChange = () => {
+        dispatch("theme-preview-change", selectedStyle);
+        themeSelection.blur();
+    };
+</script>
+
+<div id="theme-selection-container">
+    <select
+        id="theme-selection"
+        bind:value={selectedStyle}
+        bind:this={themeSelection}
+        onchange={handleChange}
+    >
+        <option value="Alps">Alps</option>
+        <option value="March">March</option>
+        <option value="Merpati">Merpati</option>
+        <option value="Tabula">Tabula</option>
+    </select>
+</div>
+
+<style>
+    #theme-selection {
+        z-index: 999;
+        position: relative;
+        cursor: pointer;
+        padding: 0.5rem;
+        background-color: white;
+        border: none;
+        border-bottom: 1px solid #e5e5e5;
+        width: 100%;
+    }
+</style>
